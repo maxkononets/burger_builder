c54be5cd2eb9b8687e76d2395d903c260f0a9554
Maksym Kononec <maksym.kononec@nixsolutions.com>
2019-11-07 16:13:59 +0200

    add .env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,4 @@
-export const appKey = 'AIzaSyBYn2ZFl4P5vK8CHslbxnL4GIvyau8iCZA';
+export const appKey = process.env.REACT_APP_FIREBASE_APP_KEY;
 export const uidIdentifier = 'localId';
 export const authTokenIdentifier = 'idToken';
 export const expireInIdentifier = 'expiresIn';
